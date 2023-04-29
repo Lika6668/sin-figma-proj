9388e4662c9ccd7b29cb2f640267cb45395eb63b
Lika6668 <luizachka8626@gmail.com>
2023-04-29 22:05:07 +0700

    add staff (pages static assets)

---
+++ pages/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-col>
+  <v-container>
+    <v-col class="main-section" align="center">
+      <video aria-label="Nothing great is made alone" 
+        role="heading" aria-level="1" 
+        src="https://cdn.sanity.io/files/599r6htc/localized/4caa8274fa1a76f5df188b7969c41e2b00890801.mp4" autoplay="" 
+        playsinline="" class="main-video"></video>
+      <h3>Figma connects everyone in the design process so teams can deliver better products, faster.</h3>
+      <v-btn class="button"
+        depressed color="primary" @click="scrollToForm">
+        Get started
+      </v-btn>
+    </v-col>
+    </v-container>
+    <div>
+      <img src="../static/main-img.png">
+    </div>
+    <v-container align="center">
+    <SocialCard/>
+    </v-container>
+    <LastDivider/>
+    <v-container ref="registerForm">
+      <RegisterForm/>
+    </v-container>
+    </v-col>
+</template>
+
+<script>
+import LastDivider from '~/components/LastDivider.vue';
+import RegisterForm from '~/components/RegisterForm.vue';
+import SocialCard from '~/components/SocialCard.vue';
+
+export default {
+   methods: {
+      scrollToForm() {
+        const el = this.$refs.registerForm;
+
+        if (el) {
+          el.scrollIntoView({ behavior: 'smooth'});
+        }
+      }
+    }
+}
+</script>
+
+ <style scoped>
+
+.col {
+  margin: 0;
+  padding: 0;
+ }
+
+ .main-section {
+    margin-bottom: 120px;
+ }
+ .main-video {
+    max-width: 80%;
+  }
+  h3 {
+    font-size: 20px;
+    max-width: 500px;
+    margin: 0px auto 40px;
+    font-weight: 400;
+  }
+
+  img  {
+    width: 110em;
+    margin-left: -10%;
+    margin-right: 0px;
+  } 
+</style>
---
+++ pages/pricing.vue
@@ -0,0 +1,142 @@
+<template>
+  <v-container>
+    <v-col align="center">
+      <PricingSelect v-on:selectType="onSelectType"/>
+    <div class="text-pricing">
+      <h1>Pricing</h1>
+    </div>
+      <div class="cards-grid">
+        <PricingCard v-bind:data="data[type][0]"/>
+        <PricingCard v-bind:data="data[type][1]"/>
+        <PricingCard v-bind:data="data[type][2]"/>
+      </div>
+    </v-col>
+  </v-container>
+</template>
+
+<script>
+import PricingCard from '~/components/PricingCard.vue';
+import PricingSelect from '~/components/PricingSelect.vue';
+
+
+export default {
+    data() {
+        return {
+            type: 0,
+            data: [
+                [
+                    {
+                        title: "Starter",
+                        price: "Free",
+                        color: "gray",
+                        time: "Forever",
+                        features: [
+                            "3 Figma and 3 FigJam files",
+                            "Unlimited personal files",
+                            "Unlimited collaborators",
+                            "Plugins, widgets, and templates",
+                            "Mobile app",
+                        ],
+                    },
+                    {
+                        title: "Starter",
+                        price: "Free",
+                        color: "green",
+                        time: "Forever",
+                        features: [
+                            "Unlimited Figma files",
+                            "Unlimited version history",
+                            "Sharing permissions",
+                            "Shared and private projects",
+                            "Team libraries",
+                            "Audio conversations",
+                        ],
+                    },
+                    {
+                        title: "Starter",
+                        price: "Free",
+                        color: "blue",
+                        time: "Forever",
+                        features: [
+                            "Org-wide libraries",
+                            "Design system analytics",
+                            "Branching and merging",
+                            "Centralized file management",
+                            "Unified admin and billing",
+                            "Private plugins and widgets",
+                            "Single sign-on",
+                        ],
+                    },
+                ],
+                [
+                    {
+                        title: "Starter",
+                        price: "Free",
+                        color: "gray",
+                        time: "Forever",
+                        features: [
+                            "3 Figma and 3 FigJam files",
+                            "Unlimited personal files",
+                            "Unlimited collaborators",
+                            "Plugins, widgets, and templates",
+                            "Mobile app",
+                            "Music player",
+                            "Ready-made templates",
+                        ],
+                    },
+                    {
+                        title: "Starter",
+                        price: "Free",
+                        color: "orange",
+                        time: "Forever",
+                        features: [
+                            "Unlimited FigJam files",
+                            "Open sessions",
+                            "Sharing permissions",
+                            "Team libraries",
+                            "Audio conversations",
+                            "Voting",
+                        ],
+                    },
+                    {
+                        title: "Starter",
+                        price: "Free",
+                        color: "violet",
+                        time: "Forever",
+                        features: [
+                            "Org-wide libraries",
+                            "Centralized file management",
+                            "Unified admin and billing",
+                            "Private plugins and widgets",
+                            "Single sign-on",
+                        ],
+                    },
+                ],
+            ],
+        };
+    },
+    methods: {
+        onSelectType(type) {
+            this.type = type;
+        }
+    },
+    components: { PricingCard, PricingSelect }
+}
+</script>
+
+ <style scoped>
+
+ .text-pricing {
+    margin-top: 70px;
+ }
+
+ h1 {
+    font-size: 60px;
+ }
+ .cards-grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 32px;
+  margin: 80px auto 40px;
+ }
+</style>
